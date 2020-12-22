1a25fdc74ced646793f1337c45b6319a00e804f4
f.baldi <f.baldi5@studenti.unipi.it>
2020-12-22 10:05:37 +0100

    first commit

---
+++ GameOn/src/main/java/it/unipi/dii/inginf/dsmt/gameon/GameOn/HelloApplication.java
@@ -0,0 +1,9 @@
+package it.unipi.dii.inginf.dsmt.gameon.GameOn;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
